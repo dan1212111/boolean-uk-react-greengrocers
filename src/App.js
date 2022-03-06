91bfbe9a32563201fd4ee0c1763b2fc810439ab1
dan1212111 <danielmc122333@icloud.com>
2022-03-06 17:27:25 +0000

    core

--- src/App.js
+++ src/App.js
@@ -1,37 +1,78 @@
 import './styles/reset.css'
 import './styles/index.css'
+import { useState } from 'react'
+import { Inventory } from './components/Inventory'
+import { ShoppingList } from './components/ShoppingList'
 
 import initialStoreItems from './store-items'
 
-/*
-Here's what a store item should look like
-{
-  id: '001-beetroot',
-  name: 'beetroot',
-  price: 0.35
-}
-
-What should a cart item look like? 🤔
-*/
-
-console.log(initialStoreItems)
-
 export default function App() {
-  // Setup state here...
+  const [items, setItems] = useState(initialStoreItems)
+  const [selectedItem, setSelectedItem] = useState([])
+  const [totalSum, setTotalSum] = useState(0)
+
+  function addToCart(item) {
+    const findItem = selectedItem.find(itemS => itemS === item)
+    if (findItem !== item) {
+      const selectedItemsList = [...selectedItem, item]
+      item.quantity = 1
+      totalCount()
+      setSelectedItem(selectedItemsList)
+      console.log(selectedItem)
+    }
+    totalCount()
+    addToQuantity(item)
+  }
+
+  function addToQuantity(item) {
+    const itemArray = [...selectedItem]
+    const itemQuantity = itemArray.find(itemQ => itemQ === item)
+    itemQuantity.quantity++
+    totalCount()
+    setSelectedItem(itemArray)
+  }
+
+  function removeFromQuantity(item) {
+    const itemArray = [...selectedItem]
+    const itemQuantity = itemArray.find(itemQ => itemQ === item)
+    if (item.quantity < 2) {
+      itemArray.splice(itemQuantity, 1)
+    }
+    itemQuantity.quantity--
+    totalCount()
+    setSelectedItem(itemArray)
+  }
+
+  let totalPrice = 0
+  
+  function totalCount() {
+    let itemQuantity = 0
+    for (const itemP of selectedItem) {
+      itemQuantity = itemP.quantity + itemQuantity
+      totalPrice = itemP.price * itemQuantity + totalPrice
+      console.log(selectedItem)
+    }
+    setTotalSum(totalPrice.toFixed(2))
+  
+  }
 
   return (
     <>
       <header id="store">
         <h1>Greengrocers</h1>
         <ul className="item-list store--item-list">
-          {/* Wrtite some code here... */}
+          <Inventory itemsList={items} addToCart={addToCart} />
         </ul>
       </header>
       <main id="cart">
         <h2>Your Cart</h2>
         <div className="cart--item-list-container">
           <ul className="item-list cart--item-list">
-            {/* Wrtite some code here... */}
+            <ShoppingList
+              selectedItem={selectedItem}
+              addToQuantity={addToQuantity}
+              removeFromQuantity={removeFromQuantity}
+            />
           </ul>
         </div>
         <div className="total-section">
@@ -39,7 +80,7 @@ export default function App() {
             <h3>Total</h3>
           </div>
           <div>
-            <span className="total-number">£0.00</span>
+            <span className="total-number">{`£${totalSum}`}</span>
           </div>
         </div>
       </main>
